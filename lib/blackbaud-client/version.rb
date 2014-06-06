ec2267c0747204e238c2618cff18c5b2f70a57c3
Alex Dugger <alexander.dugger@gmail.com>
2014-06-06 11:12:39 -0400

    Bump Version to 0.1.0

--- lib/blackbaud-client/version.rb
+++ lib/blackbaud-client/version.rb
@@ -2,8 +2,8 @@ module Blackbaud
   class Client
     class Version
       MAJOR = 0
-      MINOR = 0
-      PATCH = 5
+      MINOR = 1
+      PATCH = 0
       STRING = "#{MAJOR}.#{MINOR}.#{PATCH}"
 
       class << self
