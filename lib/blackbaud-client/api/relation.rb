f221f45050d707e5e18129d0389e41090885f725
Alex Dugger <alexander.dugger@gmail.com>
2015-04-22 10:06:06 -0400

    Relation should be using the blackbaudObject initializer

--- lib/blackbaud-client/api/relation.rb
+++ lib/blackbaud-client/api/relation.rb
@@ -1,15 +1,10 @@
 module Blackbaud
   class Relation < BlackbaudObject
-    attr_accessor(:relationship_code_id, :relationship_code_value, :reciprocal_code_id, :reciprocal_code_value, :person)
+    attr_accessor(:relationship_code_id, :relationship_code_value, :reciprocal_code_id, :reciprocal_code_value, :person, :emergency_contact)
 
     def initialize(values)
-      if values["person"]
-        values["person"] = Blackbaud::Person.new(values["person"], Blackbaud::Person::USER_TYPE[:undefined])
-      end
-
-      values.each do |k,v|
-        send("#{k}=".intern, v)
-      end
+      values["person"] = Blackbaud::Person.new(values["person"], Blackbaud::Person::USER_TYPE[:undefined]) if values["person"]
+      super(values)
     end
 
   end
