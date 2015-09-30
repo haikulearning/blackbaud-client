ce31642862093f87c4e43407c497c72080a8a720
Alex Dugger <alexander.dugger@gmail.com>
2015-09-30 12:50:40 -0400

    Update changelog and version

--- lib/blackbaud-client/version.rb
+++ lib/blackbaud-client/version.rb
@@ -2,8 +2,8 @@ module Blackbaud
   class Client
     class Version
       MAJOR = 0
-      MINOR = 1
-      PATCH = 4
+      MINOR = 2
+      PATCH = 0
       STRING = "#{MAJOR}.#{MINOR}.#{PATCH}"
 
       class << self
