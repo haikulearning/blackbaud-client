326f445d46b6f6c79d889d74d0688af2c0c61809
Alex Dugger <alexander.dugger@gmail.com>
2016-04-12 13:16:51 -0400

    Add three new attributes to the relation model.

--- lib/blackbaud-client/api/relation.rb
+++ lib/blackbaud-client/api/relation.rb
@@ -1,6 +1,6 @@
 module Blackbaud
   class Relation < BlackbaudObject
-    attr_accessor(:relationship_code_id, :relationship_code_value, :reciprocal_code_id, :reciprocal_code_value, :person, :emergency_contact)
+    attr_accessor(:relationship_code_id, :relationship_code_value, :reciprocal_code_id, :reciprocal_code_value, :person, :emergency_contact, :allow_netclassroom_view, :can_view_report_cards, :lives_with)
 
     def initialize(options)
       options.fetch(:values)["person"] = Blackbaud::Person.new(values: options.fetch(:values)["person"]) if options.fetch(:values)["person"]
